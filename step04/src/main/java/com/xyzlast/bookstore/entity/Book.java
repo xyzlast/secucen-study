f6279f7113ea96328bfc1bc66b186909fcd5441a
윤영권 <ykyoon@secucen.com>
2018-01-11 11:34:35 +0900

    [refs #9] implement Hibernate + Spring

--- step04/src/main/java/com/xyzlast/bookstore/entity/Book.java
+++ step04/src/main/java/com/xyzlast/bookstore/entity/Book.java
@@ -3,16 +3,33 @@ package com.xyzlast.bookstore.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "books")
 public class Book {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
     private int id;
+    @Column(name = "name", nullable = false, length = 255)
     private String name;
+    @Column(name = "comment", nullable = true, length = 255)
     private String comment;
+    @Column(name = "author", nullable = false, length = 50)
     private String author;
+    @Column(name = "publishDate", nullable = false)
     private Date publishDate;
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "status", nullable = false)
     private BookStatus bookStatus;
-    private Integer rentUserId;
+    @ManyToOne
+    @JoinColumn(name = "rentUserId", nullable = true)
+    private User rentUser;
+    @OneToMany(mappedBy = "book")
+    private List<History> histories;
 }
